be1f5938a97bf3fa797619dd7c21d2b0a5415152
qhj <54980372+MelancholyAstronaut@users.noreply.github.com>
2020-10-19 11:45:15 +0800

    增加了layer vuex
    Signed-off-by: qhj <54980372+MelancholyAstronaut@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,17 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from "./store/store"
+
 //
 import router from "./router/router";
-
+import axios from "axios"
 //bootstrap + jq
 import "jquery"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap'
+//vue + layer
+import layer from 'vue-layer'
+import 'vue-layer/lib/vue-layer.css';
+Vue.prototype.$layer = layer(Vue);
 
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
 new Vue({
     router,
+    store,
     render: h => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 const webpack = require("webpack");
+const path = require("path")
+
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
 
 module.exports = {
+    // assetsDir:"static",
     lintOnSave: false,
     configureWebpack: {
         plugins: [
@@ -9,5 +15,16 @@ module.exports = {
                 jQuery: 'jquery',
                 'window.jQuery': 'jquery'
             })]
-    }
+    },
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:9999',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ""
+                }
+            }
+        }
+    },
 }
