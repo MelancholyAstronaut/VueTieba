06aef6961ce503abcb3afdec07eee46358ec82a7
qhj <54980372+MelancholyAstronaut@users.noreply.github.com>
2020-10-20 11:13:32 +0800

    new_list 热门动态
    Signed-off-by: qhj <54980372+MelancholyAstronaut@users.noreply.github.com>

---
+++ src/components/page/new_list.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="new_list">
+    <div class="new_list_head clearfix">
+      <ul class="sub_nav_list">
+        <li style="margin-left: 8px;">
+          <a href="#">热门动态</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "newList",
+  components: {}
+}
+</script>
+
+<style scoped lang="scss">
+@import "src/assets/scss/newListVue";
+</style>
