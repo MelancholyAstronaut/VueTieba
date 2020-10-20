c7c81ee8dac75176d5a4fb280cf1acf1e8d9d4e6
qhj <54980372+MelancholyAstronaut@users.noreply.github.com>
2020-10-20 17:49:34 +0800

    导入qs
    Signed-off-by: qhj <54980372+MelancholyAstronaut@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from "./store/store"
 //
 import router from "./router/router";
 import axios from "axios"
+import qs from "qs";
 //bootstrap + jq
 import "jquery"
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -12,10 +13,11 @@ import 'bootstrap/dist/js/bootstrap'
 //vue + layer
 import layer from 'vue-layer'
 import 'vue-layer/lib/vue-layer.css';
+import Router from "vue-router";
 Vue.prototype.$layer = layer(Vue);
 
 Vue.prototype.$axios = axios
-
+Vue.prototype.qs = qs           //全局注册，使用方法为:this.qs
 Vue.config.productionTip = false
 
 new Vue({
