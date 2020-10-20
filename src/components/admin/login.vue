aa9951655d101b47e42ba89e21c2d734a96bb83e
qhj <54980372+MelancholyAstronaut@users.noreply.github.com>
2020-10-20 17:51:06 +0800

    登录功能实现
    Signed-off-by: qhj <54980372+MelancholyAstronaut@users.noreply.github.com>

--- src/components/admin/login.vue
+++ src/components/admin/login.vue
@@ -1,34 +1,42 @@
 <template>
-  <div id="loginVue">
-    <div class="loginDiv">
+  <div id="loginVue" class="clearfix">
+    <div class="loginDiv clearfix">
       <form>
         <p class="pass-form-logo">用户名密码登录</p>
         <div class="form-group">
           <label for="idInput"></label>
-          <input type="text" class="form-control col-md-12" id="idInput" placeholder="用户名/手机号/邮箱" v-model="username">
+          <input type="text" class="form-control col-md-12" id="idInput" placeholder="用户名/手机号/邮箱" v-model="username"
+                 v-bind:class="{passwordError :helpMsg }">
         </div>
         <div class="form-group">
-          <label for="exampleInputPassword1"></label>
-          <input type="password" class="form-control col-md-12" id="exampleInputPassword1" placeholder="密码"
-                 v-model="password">
+          <label for="InputPassword"></label>
+          <input type="password" class="form-control col-md-12" id="InputPassword" placeholder="密码"
+                 v-model="password" aria-describedby="passwordHelpBlock" v-bind:class="{passwordError :helpMsg}">
         </div>
+        <small id="passwordHelpBlock" class="form-text text-muted" v-model="helpMsg">
+          {{ helpMsg }}
+        </small>
         <router-link to="/" replace>
-          <button type="button" class="btn btn-primary" @click="submit" style="width: 100%;">登录</button>
+          <button type="button" class="btn btn-primary" @click="submit" style="width: 100%;">{{ tips }}
+          </button>
         </router-link>
-        <div id="submitWrapper">
-          <router-link to="/admin/forget-password">
-            <a class="forget-password btn">忘记密码</a>
-          </router-link>
-          <router-link to="/admin/reg-link">
-            <a class="reg-link btn">注册</a>
-          </router-link>
-        </div>
       </form>
+    </div>
+    <div id="tang-pass-footerBar">
+      <router-link to="/admin/forget-password">
+        <a class="forget-password btn">忘记密码</a>
+      </router-link>
+      <router-link to="/admin/reg-link">
+        <a class="reg-link btn">立即注册</a>
+      </router-link>
     </div>
   </div>
 </template>
 
 <script>
+import {mapMutations} from 'vuex'
+import qs from "qs";
+
 export default {
   name: "login",
   props: {
@@ -46,17 +54,49 @@ export default {
   data: function () {
     return {
       username: null,
-      password: null
+      password: null,
+      helpMsg: null,
+      tips: "登录",
     }
   },
   methods: {
+    ...mapMutations(['login']),
     submit(event) {
-      this.$layer.msg("登录成功", (layerid) => {
-        this.$layer.close(layerid);
-        this.$layer.close(this.layerid);
+      if (this.username === null) {
+        $("#idInput").focus();
+        this.$layer.msg("请填写用户名");
+        return
+      }
+      if (this.password === null) {
+        $("#InputPassword").focus();
+        this.$layer.msg("请填写密码");
+        return
+      }
+      this.tips = "登陆中......"
+      this.$axios.post("/api/user/login", this.qs.stringify({
+        name: this.username,
+        password: this.password,
+      })).then(res => {
+        if (res.status === 200 && res.data !== "") {
+          this.$layer.msg(`欢迎你 ${res.data.name}`, (layerid) => {
+            this.$layer.close(layerid);
+            this.$layer.close(this.layerid);
+            window.sessionStorage.setItem("user", qs.stringify(res.data))
+            this.login() //提交登录标记
+          })
+        } else if (res.data === "") {
+          this.$data.helpMsg = "用户名或密码错误"
+          setTimeout(() => {
+            this.username = ""
+            this.password = ""
+            this.$data.helpMsg = ""
+            this.tips = "重新登录"
+          }, 2000)
+        }
       })
-    },
+    }
   }
+
 }
 
 </script>
