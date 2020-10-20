0a7745264ef32d5608f5ae8f1cf5cf4caec41d22
qhj <54980372+MelancholyAstronaut@users.noreply.github.com>
2020-10-20 11:12:50 +0800

    登录组件实现
    Signed-off-by: qhj <54980372+MelancholyAstronaut@users.noreply.github.com>

--- src/components/admin/login.vue
+++ src/components/admin/login.vue
@@ -5,14 +5,15 @@
         <p class="pass-form-logo">用户名密码登录</p>
         <div class="form-group">
           <label for="idInput"></label>
-          <input type="text" class="form-control col-md-12" id="idInput" placeholder="用户名/手机号/邮箱">
+          <input type="text" class="form-control col-md-12" id="idInput" placeholder="用户名/手机号/邮箱" v-model="username">
         </div>
         <div class="form-group">
           <label for="exampleInputPassword1"></label>
-          <input type="password" class="form-control col-md-12" id="exampleInputPassword1" placeholder="密码">
+          <input type="password" class="form-control col-md-12" id="exampleInputPassword1" placeholder="密码"
+                 v-model="password">
         </div>
         <router-link to="/" replace>
-          <button type="button" class="btn btn-primary col-md-2" @click="submit">登录</button>
+          <button type="button" class="btn btn-primary" @click="submit" style="width: 100%;">登录</button>
         </router-link>
         <div id="submitWrapper">
           <router-link to="/admin/forget-password">
@@ -30,58 +31,36 @@
 <script>
 export default {
   name: "login",
-  methods: {
-    submit: function (event) {
-      //查数据库
+  props: {
+    info: {
+      type: Object,
+      default: () => {
+        return {};
+      }
+    },
+    layerid: {
+      type: String,
+      default: ""
+    },
+  },
+  data: function () {
+    return {
+      username: null,
+      password: null
     }
+  },
+  methods: {
+    submit(event) {
+      this.$layer.msg("登录成功", (layerid) => {
+        this.$layer.close(layerid);
+        this.$layer.close(this.layerid);
+      })
+    },
   }
 }
 
 </script>
 
-<style scoped>
-/*#loginVue{*/
-/*  width: 100%;*/
-/*  height: 738px;*/
-/*  background: #bc8f8f;*/
-/*}*/
-.loginDiv {
-  width: 356px;
-  height: 410px;
-  /*border: beige solid 2px;*/
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-}
-
-.btn-primary {
-  margin-top: 20px;
-  width: 100%;
-  height: 40px;
-  background-color: #3f89ec;
-}
-
-.pass-form-logo {
-  background: url("https://passport.baidu.com/passApi/img/loginlogo.png") no-repeat left;
-  height: 32px;
-  padding-left: 118px;
-  line-height: 46px;
-}
-
-input {
-  transition: 0.3s;
-}
-
-#submitWrapper {
-  background-color: #f0f6ff;
-  margin-top: 30px;
-  display: flex;
-  justify-content: space-between;
-}
-
-.forget-password, .reg-link {
-  font-size: 12px;
-  margin-top: 12px;
-}
+<style scoped lang="scss">
+@import "src/assets/scss/loginVue";
 </style>
