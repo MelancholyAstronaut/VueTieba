8aa166c638df0143d66814cc7380518e624977e2
qhj <54980372+MelancholyAstronaut@users.noreply.github.com>
2020-10-20 17:49:52 +0800

    实现登录状态
    Signed-off-by: qhj <54980372+MelancholyAstronaut@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,26 @@
 </template>
 <script>
 import HeadVue from "@/components/public/head-vue";
+import {mapState, mapMutations} from "vuex"
 
 export default {
   name: 'App',
   components: {HeadVue},
+  computed: {
+    ...mapState(['isLogin'])
+  },
+  methods: {
+    ...mapMutations(['login', 'logout']),
+  },
+  created: function () {
+    const isLogin = window.sessionStorage.getItem("user") || false;
+    if (isLogin === false) {
+      this.logout();
+    } else {
+      this.login();
+    }
+
+  }
 }
 </script>
 
