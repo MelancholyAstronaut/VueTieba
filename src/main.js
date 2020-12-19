c7f631582cd213c8c5b27b374539ea47665c636e
xu <xu-jiangwei515@xu>
2020-12-19 16:19:44 +0800

    取消引入的layer
    Signed-off-by: xu <xu-jiangwei515@xu>

--- src/main.js
+++ src/main.js
@@ -10,13 +10,11 @@ import qs from "qs";
 //bootstrap + jq
 import "jquery"
 //vue + layer
-import layer from 'vue-layer'
 import 'vue-layer/lib/vue-layer.css';
 //elememt-ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
-Vue.prototype.$layer = layer(Vue);
 Vue.prototype.$axios = axios
 Vue.prototype.qs = qs           //全局注册，使用方法为:this.qs
 Vue.config.productionTip = false
